4a769fde4731022874849fd0473cc1c36f47ad91
ivancsikkrisztian <kriszke20@gmail.com>
2016-04-04 14:51:17 +0200

    Uploading the first commit of my tool.

---
+++ PhoneBook/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class Person : IEnumerable
+    {
+        public Person()
+        {
+
+        }
+        public Person(string name, string address, int age, int mobile, Type type)
+        {
+            this.name = name;
+            this.address = address;
+            this.age = age;
+            this.mobile = mobile;
+            this.type = type;
+        }
+        public string name { get; set; }
+        public string address { get; set; }
+        public int age { get; set; }
+        public int mobile { get; set; }
+        public enum Type
+        {
+            Home, Workplace, Mobile
+        }
+        public Type type { get; set; }
+
+        public override string ToString()
+        {
+            return (String.Format("name: {0}, address: {1}, age: {2}, mobile: {3}", name , address, age, mobile));
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ PhoneBook/Phonebook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static PhoneBook.Person;
+
+namespace PhoneBook
+{
+    public partial class Phonebook : Form
+    {
+        List<Person> myList = new List<Person>();
+        public Phonebook()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedText == "Mobile")
+            {
+                Person human = new Person(textBox1.Text, textBox2.Text, Int32.Parse(textBox3.Text), Int32.Parse(textBox4.Text), Person.Type.Mobile);
+                myList.Add(human);
+            }
+            else if (comboBox1.SelectedText == "Home")
+            {
+                Person human = new Person(textBox1.Text, textBox2.Text, Int32.Parse(textBox3.Text), Int32.Parse(textBox4.Text), Person.Type.Home);
+                myList.Add(human);
+            }
+            else if (comboBox1.SelectedText == "Workplace")
+            {
+                Person human = new Person(textBox1.Text, textBox2.Text, Int32.Parse(textBox3.Text), Int32.Parse(textBox4.Text), Person.Type.Workplace);
+                myList.Add(human);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Person person in myList)
+            {
+                MessageBox.Show("name:" + person.name + ", address:" + person.address + ", age: " + person.age + ", mobile:" + person.mobile);
+            }
+        }
+    }
+}
